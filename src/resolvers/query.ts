b73b01e0aca6ad79c060958b6f11c29f0a7a8a5b
Momfus <momfus@outlook.com>
2020-07-18 18:32:44 -0300

    agregado consulta de un estudiando por ID - clase 59

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -5,8 +5,11 @@ import { database } from '../data/data.store';
 const query : IResolvers =  {
 
     Query: {
-        estudiantes(): any {
+        estudiantes(): any { // Lista
             return database.estudiantes;
+        },
+        estudiante( __: void, {id} ): any { // Por ID
+            return database.estudiantes.filter( estudiante => estudiante.id == id )[0]; // Se busca solo el primero, aca es obvio que sera uno pero de esta forma nos da el objeto dentro del array que devuelve
         }
     }
 
