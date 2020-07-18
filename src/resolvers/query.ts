0b87236fec99e42fb3a60d519568737e7e86263f
Momfus <momfus@outlook.com>
2020-07-18 18:35:17 -0300

    agregado si hay un estudiante desconocido

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -9,8 +9,21 @@ const query : IResolvers =  {
             return database.estudiantes;
         },
         estudiante( __: void, {id} ): any { // Por ID
-            return database.estudiantes.filter( estudiante => estudiante.id == id )[0]; // Se busca solo el primero, aca es obvio que sera uno pero de esta forma nos da el objeto dentro del array que devuelve
+            const resultado =  database.estudiantes.filter( estudiante => estudiante.id == id )[0]; // Se busca solo el primero, aca es obvio que sera uno pero de esta forma nos da el objeto dentro del array que devuelve
+            
+            // En caso de que no hya resultado
+            if( resultado == undefined ) {
+
+                return {
+                    id: '-1',
+                    name: `No se ha encontrado el estudiante con el ID ${id}`,
+                    email: '',
+                    courses: []
+                }
+
+            }
         }
+
     }
 
 }
