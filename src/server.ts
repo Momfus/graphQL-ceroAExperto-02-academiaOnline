da8b69c8c349f4ea1146d9e05ee093efe32e274c
Momfus <momfus@outlook.com>
2020-07-18 17:57:40 -0300

    creado el apollo server express

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,37 @@
 // console.log('Hola a la academia online!');
 
+import { ApolloServer } from 'apollo-server-express';
 import express from 'express';
 import compression from 'compression';
 import cors from 'cors';
 import { createServer } from 'http';
+import schema from './schema/index';
 
+import expressPlayground from 'graphql-playground-middleware-express';
 
 const app = express();
 
 app.use('*', cors()); 
 app.use( compression() );
 
-// Ruta principal
-app.get('/', (req, res) => {
+// Definir el servidor ApolloServer
+const servidor = new ApolloServer({
 
-    res.send('Hola a la academia online en GraphQl');
+    schema,
+    introspection: true
 
-});
+})
+
+
+// Definir el middleware
+servidor.applyMiddleware({app});
+
+// Ruta principal (para realizar el playground de express)
+app.get('/', expressPlayground ({
+
+    endpoint: '/graphql'
+
+}));
 
 // Inicializar servidor http
 const httpServer = createServer(app);
@@ -27,6 +42,6 @@ httpServer.listen(
     port: PORT
     },
     () => {
-        console.log(`Servidor academia online listo http://localhost:${PORT}`);
+        console.log(`Servidor academia online listo http://localhost:${PORT}/graphql`);
     },
 );
