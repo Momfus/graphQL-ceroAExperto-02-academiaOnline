82bf77be5395b3b20091b23620c716884acdc3a6
Momfus <momfus@outlook.com>
2020-07-18 22:58:53 -0300

    agregado consulta de curso por id - clase 61

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -12,7 +12,7 @@ const query : IResolvers =  {
             const resultado =  database.estudiantes.filter( estudiante => estudiante.id == id )[0]; // Se busca solo el primero, aca es obvio que sera uno pero de esta forma nos da el objeto dentro del array que devuelve
             
             // En caso de que no hya resultado
-            if( resultado == undefined ) {
+            if( resultado === undefined ) {
 
                 return {
                     id: '-1',
@@ -22,10 +22,39 @@ const query : IResolvers =  {
                 }
 
             }
+
+            return resultado;
+
         },
         cursos(): any { // Lista de cursos
             return database.cursos;
         },
+        curso( __: void, {curso} ): any { // Por ID
+            const resultado =  database.cursos.filter( curso_ => curso_.id == curso )[0]; // Se busca solo el primero, aca es obvio que sera uno pero de esta forma nos da el objeto dentro del array que devuelve
+            
+            // En caso de que no hya resultado
+            if( resultado === undefined ) {
+
+                return {
+                    id: '-1',
+                    title: `No se ha encontrado el curso con el ID ${curso}`,
+                    description: '',
+                    clases: -1,
+                    time: 0.0,
+                    logo: '',
+                    level: "TODOS",
+                    path: '',
+                    teacher: '',
+                    reviews: []
+                }
+
+            }
+
+            return resultado;
+
+        },
+
+
 
     }
 
