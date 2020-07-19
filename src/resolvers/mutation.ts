f568e325477e1d0ebc1cb9ffb3aac8a796e876b9
Momfus <momfus@outlook.com>
2020-07-19 00:47:53 -0300

    añadido la base de mutation para modificar cursos - clase 64

---
+++ src/resolvers/mutation.ts
@@ -0,0 +1,10 @@
+// Las modificaciones y eliminar
+import { IResolvers } from 'graphql-tools';
+import { database } from '../data/data.store';
+import _ from 'lodash'; // permite facilitarnos la búsqueda y filtrado en arrays (se usa el guión bajo como estándar)
+
+const mutation : IResolvers =  {
+
+}
+
+export default mutation;
