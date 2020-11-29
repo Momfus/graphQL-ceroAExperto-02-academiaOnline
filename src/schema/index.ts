703f2ad160136a338f4cbca8b76695877b0857e0
Momfus <momfus@outlook.com>
2020-11-29 19:53:12 -0300

    Añadido las instalaciones de graphql-tools necesarias - Clase 290

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,8 +1,17 @@
 import 'graphql-import-node'; // para importar el fichero de graphql
-import typeDefs from './schema.graphql';
+// import typeDefs from './schema.graphql';
 import { GraphQLSchema } from 'graphql';
 import { makeExecutableSchema } from 'graphql-tools';
 import resolvers from '../resolvers/resolversMap'
+
+// ./graphql/typeDefs.js
+import path from 'path';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import  {mergeTypeDefs}  from '@graphql-tools/merge';
+
+const typesArray = loadFilesSync(path.join(__dirname, './**/*.graphql')); // Para detectar cualquier directorio y fichero con esta extención
+
+const typeDefs = mergeTypeDefs(typesArray);
 
 const schema: GraphQLSchema = makeExecutableSchema({
 
